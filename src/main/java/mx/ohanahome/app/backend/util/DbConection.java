94debb43f19d3d71a415dd94b46f23c6b22277e0
Ever <ever.cedillo@outlook.com>
2016-04-03 16:10:48 -0500

    Example select

--- src/main/java/mx/ohanahome/app/backend/util/DbConection.java
+++ src/main/java/mx/ohanahome/app/backend/util/DbConection.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
  */
 public class DbConection {
     public static final String USER = "gae";
-    public static final String PASSWORD = "gae_pwd";
+    public static final String PASSWORD = "gae_psw";
     public static final String DRIVER = "com.mysql.jdbc.GoogleDriver";
     public static final String PROJECT = "jdbc:google:mysql://moh-333:test/";
 
@@ -29,14 +29,15 @@ public class DbConection {
 
     public static ResultSet select(String table, String columns[], String whereargs) throws SQLException {
         StringBuilder builder=new StringBuilder();
-        builder.append("SELECT");
-        for(String s: columns) {
-            builder.append(s);
-            builder.append(",");
+        builder.append("SELECT ");
+        for(int i=0; i< columns.length;i++) {
+            builder.append(columns[i]);
+            if(i!=columns.length-1)
+                builder.append(",");
         }
-        builder.append("FROM");
+        builder.append(" FROM ");
         builder.append(table);
-        builder.append("WHERE");
+        builder.append(" WHERE ");
         builder.append(whereargs);
 
         return connection.createStatement().executeQuery(builder.toString());
